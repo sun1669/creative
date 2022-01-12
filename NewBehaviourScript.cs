e9fe950933eddc668694751ff00ef4d110f6051c
alagu-soundari <76518570+alagu-soundari@users.noreply.github.com>
2022-01-12 15:23:25 +0100

    Add files via upload

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public GameObject Panel;
+
+    public void OpenPanel()
+     {
+        if(Panel != null)
+         {
+        Panel.SetActive(true);
+    }
+    }
+
+        public void LoadStartScene()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+
+   public void LoadMenuScene()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+      public void LoadMapLocationTajScene()
+    {
+        SceneManager.LoadScene("MapLocationTajScene");
+    }
+
+      public void LoadTajPointCameraScene()
+    {
+        SceneManager.LoadScene("TajPointCameraScene");
+    }
+
+       public void LoadTajMahalScene()
+    {
+        SceneManager.LoadScene("TajMahalScene");
+    }
+
+       public void LoadChinaMapLocationScene()
+    {
+        SceneManager.LoadScene("ChinaMapLocationScene");
+    }
+
+      public void LoadGreatWallPointCameraScene()
+    {
+        SceneManager.LoadScene("GreatWallPointCameraScene");
+    }
+
+   public void LoadGreatWallChinaScene()
+    {
+        SceneManager.LoadScene("GreatWallchinaScene");
+    }
+
+   public void LoadPyramidMapLocationScene()
+    {
+        SceneManager.LoadScene("PyramidMapLocationScene");
+    }
+
+   public void LoadPyramidPointCameraScene()
+    {
+        SceneManager.LoadScene("PyramidPointCameraScene");
+    }
+
+   public void LoadPyramidScene()
+    {
+        SceneManager.LoadScene("PyramidScene");
+    }
+
+
+  public void LoadPetraMapLocationScene()
+    {
+        SceneManager.LoadScene("PetraMapLocationScene");
+    }
+
+       public void LoadPetraPointCameraScene()
+    {
+        SceneManager.LoadScene("PetraPointCameraScene");
+    }
+
+   public void LoadPetraScene()
+    {
+        SceneManager.LoadScene("PetraScene");
+    }
+
+  public void LoadMachuPichuMapLocationScene()
+    {
+        SceneManager.LoadScene("MachuPichuMapLocationScene");
+    }
+       public void LoadMachuPichuPointCameraScene()
+    {
+        SceneManager.LoadScene("MachuPichuPointCameraScene");
+    }
+
+   public void LoadMachuPichuScene()
+    {
+        SceneManager.LoadScene("MachuPichuScene");
+    }
+
+
+  public void LoadChristMapLocationScene()
+    {
+        SceneManager.LoadScene("ChristMapLocationScene");
+    }
+
+       public void LoadChristPointCameraScene()
+    {
+        SceneManager.LoadScene("ChristPointCameraScene");
+    }
+
+   public void LoadChristScene()
+    {
+        SceneManager.LoadScene("ChristScene");
+    }
+
+
+  public void LoadColosseumMapLocationScene()
+    {
+        SceneManager.LoadScene("ColosseumMapLocationScene");
+    }
+       public void LoadColosseumPointCameraScene()
+    {
+        SceneManager.LoadScene("ColosseumPointCameraScene");
+    }
+
+   public void LoadColosseumScene()
+    {
+        SceneManager.LoadScene("ColosseumScene");
+    }
+
+
+
+
+    public void ExitApp()
+    {
+        Application.Quit();
+        Debug.Log("You have quit the app");
+    }
+
+    //public void LoadMenuScene()
+    //{
+      //  SceneManager.LoadScene("MenuScene");
+    //}
+}
---
+++ rotateScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateScript : MonoBehaviour
+{
+    public GameObject objectRotate;
+
+    public float rotateSpeed = 50f;
+    bool rotateStatus = false;
+
+    public void RotateObject()
+      {
+        if(rotateStatus==false)  {
+            rotateStatus = true;
+        
+    }
+    else  {
+        rotateStatus = false;
+        
+    }
+
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(rotateStatus == true)  {
+        objectRotate.transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
+    }
+        
+    }
+}
